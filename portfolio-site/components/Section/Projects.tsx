c8ae768c0e89dc7f0b060d4310ba32203ebf47ac
Jess Raphael <jxssraphael11@gmail.com>
2022-01-19 17:46:37 +0800

    Update Projects.tsx

--- portfolio-site/components/Section/Projects.tsx
+++ portfolio-site/components/Section/Projects.tsx
@@ -6,7 +6,7 @@ import Button from "../Layout/Button";
 const Features: FC = ({ children }) => {
   return (
     <div className="space mx-8 overflow-hidden">
-      <div id="features" className="relative -top-24"></div>
+      <div id="projects" className="relative -top-24"></div>
       <div className="h-[80vh] lg:h-auto flex flex-col justify-center items-center">
         <h1 className="text-6xl pt-8 py-4 font-semibold text-center text-white">
           Features
