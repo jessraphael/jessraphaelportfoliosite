caee6028797d6a723f71cf6cac72469e7f9823a2
Jess Raphael <jxssraphael11@gmail.com>
2023-08-16 12:52:39 +0800

    updated data

--- portfolio-site/components/Section/Landing.tsx
+++ portfolio-site/components/Section/Landing.tsx
@@ -23,20 +23,20 @@ const LandingPage: FC = () => {
   return (
     <div>
       <div id="home" className="absolute -top-24"></div>
-      <div className="flex flex-col items-center justify-center w-full h-screen bg-landing">
+      <div className="bg-landing flex h-screen w-full flex-col items-center justify-center">
         <div
           className={`flex items-center ${
             offset > 100 ? "opacity-20" : ""
           } transition-all duration-300`}
         >
-          <div className="grid md:grid-cols h-full">
-            <div className="flex justify-center items-center px-10 h-full">
-              <h1 className="font-semibold text-center md:text-left">
+          <div className="md:grid-cols grid h-full">
+            <div className="flex h-full items-center justify-center px-10">
+              <h1 className="text-center font-semibold md:text-left">
                 <Fade direction="down" delay={500}>
                   <p className="title-1 w-full">Hi I am</p>
                   <span className="title-2 w-full">Jess Raphael Ong</span>
                   <p className="subtext-1">
-                    Software Developer,  Adventurer,  Aspiring Entrepreneur
+                    Software Engineer, Adventurer, Aspiring Technologist
                   </p>
                 </Fade>
                 <br />
--- portfolio-site/components/data.tsx
+++ portfolio-site/components/data.tsx
@@ -1,118 +1,97 @@
-
-
-
-
 const userData = {
-    githubUsername: "jessraphael",
-    name: "Jess Raphael",
-    designation: "Software Developer",
-    avatarUrl: "/avatar.png",
-    email: "jessraphaelong@gmail.com",
-    address: "Singapore",
+  githubUsername: "jessraphael",
+  name: "Jess Raphael",
+  designation: "Software Developer",
+  avatarUrl: "/avatar.png",
+  email: "jessraphaelong@gmail.com",
+  address: "Singapore",
 
-
-
-    projects: [
-      {
-        title: "PayMeLahBot",
-        link: "https://paymelah.vercel.app/",
-        imgUrl: "/paymepls.png",
-        description: "A fully functional Telegram bot and Web App designed to help users simplify bill splitting collection among their friends by just scanning your receipt using OCR technology."
-      },
-      {
-        title: "Ai",
-        link: "https://dlw-hackathon.vercel.app/",
-        imgUrl: "/ai.png",
-        description:"Using Natural Language Processing, this product was targeted to help single individuals by analysing your chat history and help understand your potential partner's love interest in you!  [In Progress] "
-      },
-      {
-        title: "SeeTurtle",
-        link: "https://wth-seeturtle.vercel.app/",
-        imgUrl: "/seeturtle.png",
-        description:"As a team of 5, we designed a model of an AI-powered battery electric robotic turtle that aims to reduce oceanic waste by collecting microplastic crystals naked to the marine wildlife. We won the Best Design at the SUTD What The Hack 2021."
-      },
-
-      {
-        title: "PigUp",
-        link: "https://docs.google.com/document/d/1m0Cy2z7Yo9B_47V0laykZBgTgaazrCAIgX2b-tlCdCs/edit#heading=h.z6ne0og04bp5",
-        imgUrl: "/pigup.png",
-        description:"Developed an android application with my pal Amos to help youths make wise financial decisions by tracking expenses in a fun way! Level up by logging spendings daily to earn coins used to feed your pet pig!"
-      },
-    ],
-    about: {
-      title:
-        "I'm an aspiring software developer passionate to better lives through innovation",
-      description: [
-        `I am a self-starter and enthusiast of the technology sphere and a firm believer of experiential learning.`,
-        `As an adventurer, I am keen to broaden new insights and garner valuable lessons.
-        I put forth my hopes of a fruitful journey as I embark on a journey to explore the world and meet like-minded friends!`
-       ,
-      ],
+  projects: [
+    {
+      title: "PayMeLahBot",
+      link: "https://paymelah.vercel.app/",
+      imgUrl: "/paymepls.png",
+      description:
+        "A fully functional Telegram bot and Web App designed to help users simplify bill splitting collection among their friends by just scanning your receipt using OCR technology.",
     },
-    
-    experience: [
-      {
-        title: "Product Management Intern",
-        company: "SeaMoney (Shopee)",
-        year: "2022",
-        companyLink: "https://www.seamoney.com/",
-        desc: "Regional Credit Business Product Team (SeaMoney & Shopee) for 3 months from May 2022 till Aug 2022. Spearheaded enhancement features for ShopeePayLater Malaysia & Philippines. ",
-      },
-      {
-        title: "Investment Associate",
-        company: "Protege Ventures",
-        year: "2021 Aug-Present",
-        companyLink: "https://protege.vc/",
-        desc: "SEA's 1st Student Venture Capital Fund. Analysed student startups in the pre-seed stage while performing deals sourcing and due diligence as a student venture capitalist",
-      },
-      {
-        title: "Software Engineering Intern",
-        company: "Sentient.io",
-        year: "2021",
-        companyLink: "https://www.sentient.io/en/",
-        desc: "Frontend Web Developer for ScribeRabbit Video Transcription & Subtitling Service Site for 6 months from Aug 2021 till Jan 2022",
-      },
-      {
-        title: "National University of Singapore",
-        company: "School of Computing",
-        year: "2020",
-        companyLink: "https://www.comp.nus.edu.sg/",
-        desc: "Currently pursuing the Bachelor's degree in Information Systems. Expected graduation : 2024",
-      },
-      {
-        title: "SAF Commandos",
-        company: "National Service",
-        year: "2018",
-        companyLink: "",
-        desc: "Detachment Sergeant. \nAwarded the Best Combat Unit (WY 18/19) and the Regimental Sergeant Major Coin for outstanding performance."
-      },
-      {
-        title: "Advanced Level",
-        company: "Nanyang Junior College",
-        year: "2016",
-        companyLink: "https://nanyangjc.moe.edu.sg/",
-        desc: "Completed GCE A-Levels with 3 Distinctions. The subjects read are H2 Physics, H2 Economics, H2 Mathematics, H1 Chemistry, H1 Project Work, H1 General Paper",
-      },
-      {
-        title: "High School",
-        company: "Maris Stella High School",
-        year: "2012",
-        companyLink: "https://marisstellahigh.moe.edu.sg/",
-        desc: "7 Distinctions",
-      },
-    ],
-    resumeUrl:
-      "",
-
-    socialLinks: {
-      instagram: "https://www.instagram.com/jxphxel_/",
-      linkedin: "https://www.linkedin.com/in/jessraphael/",
-      github: "https://github.com/jessraphael",
-      facebook: "https://www.facebook.com/jessongweihao"
+    {
+      title: "Ai",
+      link: "https://dlw-hackathon.vercel.app/",
+      imgUrl: "/ai.png",
+      description:
+        "Using Natural Language Processing, this product was targeted to help single individuals by analysing your chat history and help understand your potential partner's love interest in you!  [In Progress] ",
     },
-  };
-  
-  export default userData;
-  
+    {
+      title: "SeeTurtle",
+      link: "https://wth-seeturtle.vercel.app/",
+      imgUrl: "/seeturtle.png",
+      description:
+        "As a team of 5, we designed a model of an AI-powered battery electric robotic turtle that aims to reduce oceanic waste by collecting microplastic crystals naked to the marine wildlife. We won the Best Design at the SUTD What The Hack 2021.",
+    },
 
-  
+    {
+      title: "PigUp",
+      link: "https://docs.google.com/document/d/1m0Cy2z7Yo9B_47V0laykZBgTgaazrCAIgX2b-tlCdCs/edit#heading=h.z6ne0og04bp5",
+      imgUrl: "/pigup.png",
+      description:
+        "Developed an android application with my pal Amos to help youths make wise financial decisions by tracking expenses in a fun way! Level up by logging spendings daily to earn coins used to feed your pet pig!",
+    },
+  ],
+  about: {
+    title:
+      "I'm an aspiring software developer passionate to better lives through innovation",
+    description: [
+      `I am a self-starter and enthusiast of the technology sphere and a firm believer of experiential learning.`,
+      `As an adventurer, I am keen to broaden new insights and garner valuable lessons.
+        I put forth my hopes of a fruitful journey as I embark on a journey to explore the world and meet like-minded friends!`,
+    ],
+  },
+
+  experience: [
+    {
+      title: "Industrial Attachment (Developer)",
+      company: "Bank of America Merrill Lynch",
+      year: "2023",
+      companyLink: "https://www.bankofamerica.com/",
+      desc: "Class of 2023 Global Technology Analyst for Bank of America, Global Corporate and Investment Banking Technology for 6 months from Jan 2023 till Jun 2023",
+    },
+    {
+      title: "Product Management Intern",
+      company: "SeaMoney (Shopee)",
+      year: "2022",
+      companyLink: "https://www.seamoney.com/",
+      desc: "Regional Credit Business Product Team (SeaMoney & Shopee) for 3 months from May 2022 till Aug 2022. Spearheaded enhancement features for ShopeePayLater Malaysia & Philippines. ",
+    },
+    {
+      title: "Investment Associate",
+      company: "Protege Ventures",
+      year: "2021 Aug-Present",
+      companyLink: "https://protege.vc/",
+      desc: "SEA's 1st Student Venture Capital Fund. Analysed student startups in the pre-seed stage while performing deals sourcing and due diligence as a student venture capitalist",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "Sentient.io",
+      year: "2021",
+      companyLink: "https://www.sentient.io/en/",
+      desc: "Frontend Web Developer for ScribeRabbit Video Transcription & Subtitling Service Site for 6 months from Aug 2021 till Jan 2022",
+    },
+    {
+      title: "National University of Singapore",
+      company: "School of Computing",
+      year: "2020",
+      companyLink: "https://www.comp.nus.edu.sg/",
+      desc: "Currently pursuing the Bachelor's degree in Information Systems. Expected graduation : 2024",
+    },
+  ],
+  resumeUrl: "",
+
+  socialLinks: {
+    instagram: "https://www.instagram.com/jxphxel_/",
+    linkedin: "https://www.linkedin.com/in/jessraphael/",
+    github: "https://github.com/jessraphael",
+    facebook: "https://www.facebook.com/jessongweihao",
+  },
+};
+
+export default userData;
